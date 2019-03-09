5b0bd03b356b68d33825605b7d501d298183eeeb
naveendk <naveenkolambage@gmail.com>
2019-03-09 13:40:37 +1200

    how to access objects

--- test-object.js
+++ test-object.js
@@ -1,14 +1,39 @@
-// Setup
-var testObj = {
-    12: "Namath",
-    16: "Montana",
-    19: "Unitas"
-  };
+// // Setup
+// var testObj = {
+//     12: "Namath",
+//     16: "Montana",
+//     19: "Unitas"
+//   };
   
-  // Only change code below this line;
+//   // Only change code below this line;
   
-  var playerNumber = 16;       // Change this Line
-  var player = testObj[playerNumber]   // Change this Line
+//   var playerNumber = 16;       // Change this Line
+//   var player = testObj[playerNumber]   // Change this Line
 
-  console.log(testObj);
-  console.log(player);
+//   console.log(testObj);
+//   console.log(player);
+
+// Setup
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+
+myDog["name"]="Happy Coder";
+
+console.log(myDog);
