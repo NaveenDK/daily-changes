869dce35cb3e73dd229de5c50f3ebb69b55944c7
naveendk <naveenkolambage@gmail.com>
2019-03-10 18:21:17 +1200

    array  adding content forloop

--- test-object.js
+++ test-object.js
@@ -152,56 +152,75 @@
 // console.log(secondTree);//works
 
 // Setup
-var collection = {
-  "2548": {
-    "album": "Slippery When Wet",
-    "artist": "Bon Jovi",
-    "tracks": [ 
-      "Let It Rock", 
-      "You Give Love a Bad Name" 
-    ]
-  },
-  "2468": {
-    "album": "1999",
-    "artist": "Prince",
-    "tracks": [ 
-      "1999", 
-      "Little Red Corvette" 
-    ]
-  },
-  "1245": {
-    "artist": "Robert Palmer",
-    "tracks": [ ]
-  },
-  "5439": {
-    "album": "ABBA Gold"
-  }
-};
-// Keep a copy of the collection for tests
-var collectionCopy = JSON.parse(JSON.stringify(collection));
+// var collection = {
+//   "2548": {
+//     "album": "Slippery When Wet",
+//     "artist": "Bon Jovi",
+//     "tracks": [ 
+//       "Let It Rock", 
+//       "You Give Love a Bad Name" 
+//     ]
+//   },
+//   "2468": {
+//     "album": "1999",
+//     "artist": "Prince",
+//     "tracks": [ 
+//       "1999", 
+//       "Little Red Corvette" 
+//     ]
+//   },
+//   "1245": {
+//     "artist": "Robert Palmer",
+//     "tracks": [ ]
+//   },
+//   "5439": {
+//     "album": "ABBA Gold"
+//   }
+// };
+// // Keep a copy of the collection for tests
+// var collectionCopy = JSON.parse(JSON.stringify(collection));
 
-// Only change code below this line
-function updateRecords(id, prop, value) {
-  if(!value){
-    delete collection[id][prop];
-    return collection;
-  }
+// // Only change code below this line
+// function updateRecords(id, prop, value) {
+//   if(!value){
+//     delete collection[id][prop];
+//     return collection;
+//   }
   
-  if(prop !== "tracks"){
-    collection[id][prop] = value;
-  }
-  else{
-    if (!collection[id].hasOwnProperty("tracks")) collection[id].tracks =[];
+//   if(prop !== "tracks"){
+//     collection[id][prop] = value;
+//   }
+//   else{
+//     if (!collection[id].hasOwnProperty("tracks")) collection[id].tracks =[];
 
-    collection[id].tracks.push(value);
-  }
+//     collection[id].tracks.push(value);
+//   }
 
-  return collection;
+//   return collection;
+// }
+
+// // Alter values below to test your code
+// updateRecords(5439, "artist", "ABBA");
+// updateRecords(5439,"tracks","track1");
+// updateRecords(1245, "tracks", "Addicted to Love");
+// //updateRecords(2548,"artist","");
+// console.log(collection)
+
+// Example
+var ourArr = [ 9, 10, 11, 12];
+var ourTotal = 0;
+
+for (var i = 0; i < ourArr.length; i++) {
+  ourTotal += ourArr[i];
 }
 
-// Alter values below to test your code
-updateRecords(5439, "artist", "ABBA");
-updateRecords(5439,"tracks","track1");
-updateRecords(1245, "tracks", "Addicted to Love");
-//updateRecords(2548,"artist","");
-console.log(collection)
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+
+// Only change code below this line
+var total=0;
+for( var i = 0; i<=(myArr.length-1); i++ ){
+   total += myArr[i];
+}
+
+console.log(total);
