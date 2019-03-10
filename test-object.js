e9d6ba264682b730fd9fef449fa633bb35c22a29
naveendk <naveenkolambage@gmail.com>
2019-03-10 12:48:55 +1200

    added thetesting objects for properties

--- test-object.js
+++ test-object.js
@@ -38,24 +38,73 @@
 
 // console.log(myDog);
   // Setup
-function phoneticLookup(val) {
-  var result = "";
+// function phoneticLookup(val) {
+//   var result = "";
 
-  // Only change code below this line
- var lookup={
-  "alpha":"Adams",
-  "bravo":"Boston",
-  "charlie":"Chicago",
-  "delta":"Denver",
-  "echo":"Easy",
-  "foxtrot":"Frank"
- }
+//   // Only change code below this line
+//  var lookup={
+//   "alpha":"Adams",
+//   "bravo":"Boston",
+//   "charlie":"Chicago",
+//   "delta":"Denver",
+//   "echo":"Easy",
+//   "foxtrot":"Frank"
+//  }
 
-  result=lookup[val];
+//   result=lookup[val];
 
-  // Only change code above this line
-  return result;
+//   // Only change code above this line
+//   return result;
+// }
+
+// // Change this value to test
+// phoneticLookup("charlie");
+// Setup
+// var myObj = {
+//   gift: "pony",
+//   pet: "kitten",
+//   bed: "sleigh"
+// };
+
+// function checkObj(checkProp) {
+//   // Your Code Here
+//  if(myObj.hasOwnProperty(checkProp)=="gift")
+//   return "pony";
+//   if( myObj.hasOwnProperty(checkProp)=="pet")
+//   return "kitten";
+//   if(myObj.hasOwnProperty(checkProp)=="sleigh")
+//   return "sleigh"; 
+
+// }
+
+// // Test your code by modifying these values
+// checkObj("gift");
+
+// console.log('checkObj("gift")');
+
+// Setup
+var myObj = {
+  gift: "pony",
+  pet: "kitten",
+  bed: "sleigh"
+};
+
+function checkObj(checkProp) {
+  // Your Code Here
+  switch(checkProp){
+    case "gift":
+    return "pony";
+    break;
+    case "pet":
+    return "kitten";
+    case "bed":
+    return "sleigh";
+    default:
+    return "Not Found";
+  }
+
 }
 
-// Change this value to test
-phoneticLookup("charlie");
+// Test your code by modifying these values
+checkObj("gift");
+console.log(checkObj("gift"));
