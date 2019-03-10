42844d11268aca3074201ea1125ec9e8ca003951
naveendk <naveenkolambage@gmail.com>
2019-03-10 16:38:03 +1200

    added final solution for record collection

--- test-object.js
+++ test-object.js
@@ -126,27 +126,82 @@
 // console.log(gloveBoxContents); //works!
 
 // Setup
-var myPlants = [
-  { 
-    type: "flowers",
-    list: [
-      "rose",
-      "tulip",
-      "dandelion"
+// var myPlants = [
+//   { 
+//     type: "flowers",
+//     list: [
+//       "rose",
+//       "tulip",
+//       "dandelion"
+//     ]
+//   },
+//   {
+//     type: "trees",
+//     list: [
+//       "fir",
+//       "pine",
+//       "birch"
+//     ]
+//   }  
+// ];
+
+// // Only change code below this line
+
+// var secondTree = myPlants[1].list[1]; // Change this line
+
+// console.log(secondTree);//works
+
+// Setup
+var collection = {
+  "2548": {
+    "album": "Slippery When Wet",
+    "artist": "Bon Jovi",
+    "tracks": [ 
+      "Let It Rock", 
+      "You Give Love a Bad Name" 
     ]
   },
-  {
-    type: "trees",
-    list: [
-      "fir",
-      "pine",
-      "birch"
+  "2468": {
+    "album": "1999",
+    "artist": "Prince",
+    "tracks": [ 
+      "1999", 
+      "Little Red Corvette" 
     ]
-  }  
-];
+  },
+  "1245": {
+    "artist": "Robert Palmer",
+    "tracks": [ ]
+  },
+  "5439": {
+    "album": "ABBA Gold"
+  }
+};
+// Keep a copy of the collection for tests
+var collectionCopy = JSON.parse(JSON.stringify(collection));
 
 // Only change code below this line
+function updateRecords(id, prop, value) {
 
-var secondTree = myPlants[1].list[1]; // Change this line
+    if(!value){
+      delete collection[id][prop];
+      return collection;
+    }
 
-console.log(secondTree);
+    if(prop !== "tracks" && value){
+      collection[id][prop] = value; //can create artist prop of 5439 just like that 
+    }
+    else if (!collection[id].hasOwnProperty("tracks")){ collection[id].tracks =[]; 
+      //var arrTrack=[value];
+      collection[id].tracks.push(value);
+
+    }
+    
+return collection;
+}
+
+// Alter values below to test your code
+updateRecords(5439, "artist", "ABBA");
+updateRecords(5439,"tracks","track1");
+//updateRecords(2548,"artist","");
+console.log(collection)
